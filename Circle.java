ef46d6ff0dde7d258019205dcd23d68c449d4605
Lukas Schneider <lukasschneider@casady.org>
2018-09-28 10:46:41 -0500

    Circle Game sort of functions

--- Circle.java
+++ Circle.java
@@ -3,37 +3,43 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import java.util.*;
 import javax.swing.*;
-public class CircleGame{
+public class Circle{
   public static void main(String[] args){
     MyFrame f = new MyFrame();
+    MyPanel p = new MyPanel();
+    f.add(p);
     f.setVisible(true);
   }
 }
 
 class MyFrame extends JFrame{
  public MyFrame(){
-   super("Circle Game");
+   super("Circle");
    setSize(400,400);
    setResizable(false);
    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+   /*
    MyPanel p = new MyPanel();
    add(p);
+   */
  }
 }
 
 class MyPanel extends JPanel{
+  MyCircle basecircle = new MyCircle(20, 20, 100);
   public MyPanel(){
-    MyCircle basecircle = new MyCircle(50, -150, 10);
-    repaint();
+    // MyCircle basecircle = new MyCircle(20, 20, 10);
     addMouseMotionListener(new MouseMotionAdapter() {
       public void mouseMoved(MouseEvent e) {
         PointerInfo a = MouseInfo.getPointerInfo();
         Point b = a.getLocation();
-        int x = (int) b.getX();
-        int y = (int) b.getY();
-        if(x == basecircle.getx() && y == basecircle.gety()){
-          MyCircle newcircle = new MyCircle((int) (Math.random() * 350), (int) (Math.random() * 350), 10);
+        //int x = (int) b.getX();
+        //int y =  (int) b.getY();
+        if(Math.sqrt(Math.pow(basecircle.getx() - b.getX(), 2) + Math.pow(basecircle.gety() - b.getY(), 2)) < 50){
+            basecircle.setX((int) (Math.random() * 299));
+            basecircle.setY((int) (Math.random() * 299));
+          //basecircle = new MyCircle((int) (Math.random() * 350), (int) (Math.random() * 350), 10);
         }
           repaint();
       }
@@ -43,15 +49,16 @@ class MyPanel extends JPanel{
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D) g;
-    MyCircle circle = new MyCircle(((int)Math.random() * 299), (int)(Math.random() * 299), 5);
-    MyCircle.drawCircle(g2);
+    //MyCircle circle = new MyCircle((int)(Math.random() * 299), (int)(Math.random() * 299), 10);
+    //circle.drawCircle(g2);
+    basecircle.drawCircle(g2);
     }
   }
 
 
 class MyCircle{
-  public static int x;
-  public static int y;
+  private int x;
+  private int y;
   private int diameter;
   public MyCircle(int x,int y,int diameter){
     this.x = x;
@@ -61,10 +68,16 @@ class MyCircle{
   public void drawCircle(Graphics2D g){
     g.fillOval(x,y,diameter,diameter);
   }
-  public static int getx(){
+  public int getx(){
     return x;
   }
-  public static int gety(){
+  public int gety(){
     return y;
+  }
+  public void setX(int newX){
+      this.x = newX;
+  }
+  public void setY(int newY){
+      this.y = newY;
   }
 }
