273db8db7e4763c1d6101f61b544b622d3300af5
DataRoxas <lukasschneider@casady.org>
2018-10-06 16:05:19 -0700

    Non Functional Menu Code

--- Menu.java
+++ Menu.java
@@ -1,8 +1,10 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
 import java.util.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class Menu{
     public static void main(String[] args){
@@ -10,3 +12,70 @@ public class Menu{
       f.setVisible(true);
     }
   }
+
+private class MyFrame extends JFrame{
+
+    private MyPanel panel = new MyPanel();
+
+    public void MyFrame(){
+        panel.setBackground(Color.RED);
+        setLocation(100, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        displayMenu();
+        setSize(600, 600);
+
+
+        add(panel);
+        setResizable(false);
+    }
+
+    public void displayMenu(){
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(new QuitMenu());
+        menuBar.add(new ColorMenu());
+        setJMenuBar(menuBar);
+    }
+    private class QuitMenu extends JMenu{
+        public QuitMenu(){
+            super("Quit Menu");
+            JMenuItem quitMI = new JMenuItem("Quit");
+            quitMI.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    System.exit(0);
+                }
+            });
+            add(quitMI);
+        }
+    }
+    private class BackgroundColorMenu extends JMenu{
+        public void ColorMenu(){
+            super("Color");
+            for(Color color : Color.values()){
+                JMenuItem menuItem = new JMenuItem(color.name());
+                menuItem.addActionListener(new ColorListener(color));
+                add(menuItem);
+            }
+
+        }
+    }
+    private class ColorListener implements ActionListener{
+        private Color color;
+        public void actionPerformed(ActionEvent e){
+            panel.setBackground(color.color);
+        }
+        public ColorListener(Color color){
+            this.color = color;
+        }
+    }
+}
+    public enum Color{
+        Red(Color.RED), Blue(Color.BLUE), Green(Color.GREEN);
+        public Color color;
+
+        Color(Color color){
+            this.color = color;
+        }
+        Color color(){
+            return color;
+        }
+    }
