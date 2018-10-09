a613dc679b452db76c3cacc18a96407c27d35265
Eric Ebert <eberte@casady.org>
2018-10-09 15:49:00 -0500

    Basic Outline

--- Button.java
+++ Button.java
@@ -12,3 +12,30 @@ public class Button{
         f.setVisible(true);
     }
 }
+
+class MyFrame extends JFrame{
+    # Make the panel an attribute
+        
+    public MyFrame(){
+        # Put the keyListener
+    }
+    
+}
+
+class MyPanel extends JPanel{
+    
+    public MyPanel(){
+        # add MouseListener
+    }
+    
+    public void paintComponent(Graphics g){
+        
+    }
+    
+    public Point getRandomPoint(){
+        
+        
+    }
+    
+    # Write a method to reset the panel 
+}
