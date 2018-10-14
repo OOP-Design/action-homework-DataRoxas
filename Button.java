2619874928b014fbcf7c1f0d3f9258383da263a7
DataRoxas <lukasschneider@casady.org>
2018-10-14 11:01:22 -0700

    Some of the code added
    I'm still unsure about how to do the other stuff, do I need to call panel at all or just call removeAll() by itself?

--- Button.java
+++ Button.java
@@ -14,28 +14,45 @@ public class Button{
 }
 
 class MyFrame extends JFrame{
-    # Make the panel an attribute
-        
+        private MyPanel panel = new MyPanel();
     public MyFrame(){
+        setSize(600,600);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         # Put the keyListener
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e){
+                char c = e.getKeyChar();
+                if (charList.contains(c)){
+                    //Do I add a statement here to add the character to the panel? What would that look like? That?
+                    panel.paintComponent();
+                }
+            }
+        });
     }
-    
+
 }
 
 class MyPanel extends JPanel{
-    
+
     public MyPanel(){
-        # add MouseListener
+        addMouseListener(new MouseAdapter() {
+            public void MouseClicked(MouseEvent e){
+                panel.removeAll();
+
+            }
+        })
     }
-    
+
+
     public void paintComponent(Graphics g){
-        
+
     }
-    
+
     public Point getRandomPoint(){
         
-        
+
     }
-    
-    # Write a method to reset the panel 
+
+    # Write a method to reset the panel
 }
